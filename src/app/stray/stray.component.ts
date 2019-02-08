2650bbd2c37c1c8af953f1e7ed8cc4e04ee87699
Brandon <robertsbt@gmail.com>
2019-02-08 13:29:36 +0000

    show stray component preventing compilation

---
+++ src/app/stray/stray.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stray',
+  templateUrl: './stray.component.html',
+  styleUrls: ['./stray.component.css']
+})
+export class StrayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
