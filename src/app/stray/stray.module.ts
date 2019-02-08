ff6a449d12ceadfa09c24ff1a748840def77a10a
Brandon <robertsbt@gmail.com>
2019-02-08 13:42:30 +0000

    add repro of compilation error with one component in declarations of 2 NgModules

---
+++ src/app/stray/stray.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StrayComponent } from './stray.component';
+
+@NgModule({
+  declarations: [StrayComponent],
+  imports: [
+    CommonModule,
+  ]
+})
+export class StrayModule { }
